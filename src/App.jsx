1370e75bd073868c0c6589750c2b8b020beef020
mhdhssn967 <mhdhssn967@gmail.com>
2026-03-11 00:51:20 +0530

    some solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,8 @@ import '@/index.css'
 import '@/styles/animations.css'
 
 export default function App() {
+  document.addEventListener("pointerdown", (e) => {
+  e.target.releasePointerCapture(e.pointerId);
+}, true);
   return <Router />
 }
